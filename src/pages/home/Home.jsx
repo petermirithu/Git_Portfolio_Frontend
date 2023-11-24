eed55cd834c3da56d4a43f4f9aba718e92dea73d
John Moses <82748369+JohnMose@users.noreply.github.com>
2023-11-24 05:01:19 -0800

    Ft UI setup (#5)
    * Added a side bar
    
    * edited the homePage
    
    
    
    Co-authored-by: JemmyKuria <kuriajemmy@gmail.com>

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import Tabs from './tabs';
+import Sidebar from '../../Components/Sidebar';
 
 export default function Home() {
    
    return (
-       <>
+    <>
+    <Sidebar/>
+
        <Tabs/>
-       </>
+    </> 
    );
 }
 

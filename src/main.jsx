d6c4dfcfc71cc3d7dc7c1e95942d1c210881dd68
petermirithu <peter_mirithu@yahoo.com>
2023-11-27 18:06:31 +0300

    add roboto font

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,15 @@ import './index.css'
 import { Provider } from 'react-redux'
 import Store from './redux/Store.js'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const defaultTheme = createTheme();
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
